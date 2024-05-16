762719ed3bf9b765f7b45702f7bdb784885bae35
Danilo <danilo.ribeirodr01@gmail.com>
2024-05-16 03:10:42 -0300

    Adicionado Consulta de Disponibilidade

--- main.js
+++ main.js
@@ -57,6 +57,20 @@ const app = Vue.createApp({
       }
     },
 
+    convertDatetoBr(date){
+      const dateSplit = date.split("-")
+      return (dateSplit[2] + '/' + dateSplit[1] + '/' + dateSplit[0])
+    },
+
+    getDate(){
+      let today = new Date();
+      let dd = String(today.getDate()).padStart(2, '0')
+      let mm = String(today.getMonth() + 1).padStart(2, '0')
+      let yyyy = today.getFullYear()
+
+      return (yyyy+'-' + mm + '-' + dd )
+    },
+
     async buffetDetails(id){
       let response = await fetch(`http://127.0.0.1:3000/api/v1/buffet_registrations/${id}`)
       let data = await response.json()
@@ -88,6 +102,32 @@ const app = Vue.createApp({
       
       this.screen = this.detailsScreen
       
+    },
+
+    async consult(element){
+      const amountOfPeople = element.srcElement['amountOfPeople'].value
+      const date = element.srcElement['date'].value
+      const id = element.srcElement['id'].value
+      
+      let response = await fetch(`http://localhost:3000/api/v1/event_types/${id}/orders?date=${date}&amount_of_people=${amountOfPeople}`, {method: "POST"})
+      console.log(response.status)
+      let data = await response.json()
+      divResult = element.srcElement.nextElementSibling
+      
+      if (response.status == 200){
+        divResult.innerHTML = `
+          <h3>Valor Previsto</h3>
+          <strong>Evento para o dia:</strong> ${this.convertDatetoBr(date)} <br>
+          <strong>Quantidade de pessoas:</strong> ${amountOfPeople} <br>
+          <strong>Valor previsto:</strong> R$ ${data.prior_value.toFixed(2)}
+          `
+      }else{
+        divResult.innerHTML = `
+        <h3>Erro ao consultar a disponibilidade:</h3>
+        <strong>Motivo</strong>: ${data.errors}
+        `
+      }
+
     }
   }
 })
