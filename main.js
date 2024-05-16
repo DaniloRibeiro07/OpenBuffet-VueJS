c10d9c78441d6a22e3d2dff7248cfc1db736790c
Danilo <danilo.ribeirodr01@gmail.com>
2024-05-16 01:28:46 -0300

    Adicionado tela de detalhes de um buffet e tipos de eventos

--- main.js
+++ main.js
@@ -36,20 +36,58 @@ const app = Vue.createApp({
     },
 
     changeView(view){
-      if (view=='homeScreen'){
+      if (view == 'homeScreen'){
         this.screen = this.homeScreen
+      }
+    },
+
+    translate(text){
+      if(text == "pix"){
+        return "Pix"
+      }else if(text == "boleto"){
+        return "Boleto"
+      }else if(text == "credit_card"){
+        return "Cartão de Crédito"
+      }else if(text == "debit_card"){
+        return "Cartão de Débito"
+      }else if(text == "bitcoin"){
+        return "Bitcoin"
+      }else if(text == "money"){
+        return "Dinheiro"
       }
     },
 
     async buffetDetails(id){
       let response = await fetch(`http://127.0.0.1:3000/api/v1/buffet_registrations/${id}`)
       let data = await response.json()
+      console.log(data)
+
       this.buffet = data
       this.buffet.payment =  Object.keys(this.buffet.payment_method).filter( key => {
         return this.buffet.payment_method[key]
       })
-      this.screen = this.detailsScreen
+
+      this.buffet.payment = this.buffet.payment.map((payment) => this.translate(payment))
+
+      response = await fetch(`http://127.0.0.1:3000/api/v1/buffet_registrations/${id}/event_types`)
+      data = await response.json()
+
       console.log(data)
+
+      this.buffet.event_types = data
+
+      this.buffet.event_types.forEach(element => {
+        if (element.outsider && element.insider){
+          element.insiderOutsider = "dentro ou fora do buffet"
+        }else if(element.outsider){
+          element.insiderOutsider = "apenas fora do buffet"
+        }else{
+          element.insiderOutsider = "apenas dentro do buffet"
+        }
+      });
+      
+      this.screen = this.detailsScreen
+      
     }
   }
 })
